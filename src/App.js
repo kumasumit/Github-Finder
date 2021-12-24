64fbbcbf8e92f948ead02818b7793aa747a11e6b
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-24 11:55:05 +0530

    Completed Navabar Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+
 export default function App() {
   return (
-    <>
-      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-        <div>
-          <div class="text-xl font-medium text-black">ChitChat</div>
-          <p class="text-gray-500">You have a new message!</p>
-          <button class="btn btn-primary">daisyUI Button</button>
-        </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>Content</main>
       </div>
-    </>
+    </Router>
   );
 }
