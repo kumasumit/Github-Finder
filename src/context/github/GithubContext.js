129d9005e5f15023da4a398bc4458c1b296d0bd6
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-24 18:25:28 +0530

    Set up Gitub Context

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+
+const GithubContext = createContext();
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchUsers = async () => {
+    const response = await fetch("https://api.github.com/users", {
+      headers: {
+        Authorization: "token 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",
+      },
+    });
+
+    const data = await response.json();
+
+    setUsers(data);
+    setLoading(false);
+  };
+  return (
+    <GithubContext.Provider
+      value={{
+        users,
+        loading,
+        fetchUsers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
