5a1ee3302643f08950a1ae0157e783fa5173202d
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-24 16:42:09 +0530

    Fetch Users and User-Results Component

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
+
+function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch("https://api.github.com/users", {
+      headers: {
+        Authorization: "token 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",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+    setUsers(data);
+    setLoading(false);
+  };
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+}
+
+export default UserResults;
