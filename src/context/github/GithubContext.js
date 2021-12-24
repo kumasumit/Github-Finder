ac781d7a847aabefcd5eda599563ede16adf952b
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-24 18:56:24 +0530

    Clean Up Fetch Users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,13 +8,16 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
   const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  //Get Initial Users testing Purposes
   const fetchUsers = async () => {
-    const response = await fetch("https://api.github.com/users", {
+    setLoading();
+    const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
-        Authorization: "token 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",
+        Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
@@ -24,6 +27,9 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     });
   };
+
+  //Set Loading
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
   return (
     <GithubContext.Provider
       value={{
